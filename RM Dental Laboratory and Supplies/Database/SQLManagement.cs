249cd7171ead1d1efb9ea6ac96274fc6387d69ac
Errol Robyn Abella <it.era0211@gmail.com>
2024-03-14 09:36:58 +0800

    Added database class

---
+++ RM Dental Laboratory and Supplies/Database/SQLManagement.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RM_Dental_Laboratory_and_Supplies.Database
+{
+    public class SQLManagement
+    {
+
+    }
+}
